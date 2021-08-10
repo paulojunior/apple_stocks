93b20c968eb1d491b74c761c5f6bd9b2c51f85ff
Paulo Junior <jr@live.at>
2021-08-10 16:40:52 -0300

    Implementing StockListViewModel and StockViewModel

---
+++ StocksApp/ViewModels/StockListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  StockListViewModel.swift
+//  StocksApp
+//
+//  Created by Junior Ferreira on 10/08/21.
+//
+
+import Foundation
+
+class StockListViewModel {
+    
+    var searchTerm: String = ""
+    var stocks: [StockViewModel] = [StockViewModel]()
+}
