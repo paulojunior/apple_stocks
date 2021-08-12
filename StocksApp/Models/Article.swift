3127c053541c8d687a785b2de78f6eda93ccbd66
Paulo Junior <jr@live.at>
2021-08-12 13:09:58 -0300

    Implementing Webservice to Fetch News and Creating Models

---
+++ StocksApp/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  StocksApp
+//
+//  Created by Junior Ferreira on 12/08/21.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let imageUrl: String
+    let publication: String
+}
